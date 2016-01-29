e09ef6371ebaeac468ae022e0324bb4913e32496
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-28 21:18:35 -0500

    Generating the products controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,24 @@
+Product.create!(
+  :name  => "Drink",
+  :description => "Pepsi Cola",
+  :price_in_cents => 150
+)
+
+Product.create!(
+  :name  => "Cup",
+  :description => "Mug",
+  :price_in_cents => 250
+)
+
+Product.create!(
+  :name  => "Candy",
+  :description => "Rockets",
+  :price_in_cents => 50
+)
+
+
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
